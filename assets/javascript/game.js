481a43300616bf531ef7df673aadd78832621638
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-04-30 18:25:20 -0600

    game is working, need reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,37 +3,50 @@ const wordBank = ["espresso", "latte", "beans", "roast", "colombian", "ethiopian
 
 var charactersGuessed = [];
 var remainingGuesses = 9;
+document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 console.log(currentWord);
 
 var underScore = [];
 for (var i = 0; i < currentWord.length; i++) {
-    underScore[i] = "_";
-} 
+    underScore[i] = " _ ";
+    document.getElementById("correctWord").innerHTML = underScore;
+}
 
 var remainingLetters = currentWord.length;
 
+var wins = 0;
+document.getElementById("wins").innerHTML = wins;
 
 //Here's the actual game
-    document.onkeyup = function (event) {
-        var userGuess = event.key.toLowerCase();
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    var l = currentWord.indexOf(userGuess);
 
-        var l = currentWord.indexOf(userGuess, 0);
-        console.log(userGuess);
-
-        if (l > -1) {
-            for (var j = 0; j < currentWord.length; j++) {
-                if (currentWord[j] === userGuess) {
-                    underScore[j] = userGuess;
-                    document.getElementById("correctWord").innerHTML = underScore.join(" ");
-                    remainingLetters--;
+console.log(userGuess);
+    if (l > -1) {
+        for (var j = 0; j < currentWord.length; j++) {
+            if (currentWord[j] === userGuess) {
+                underScore[j] = userGuess;
+                document.getElementById("correctWord").innerHTML = underScore.join(" ");
+                remainingLetters--;    
+                } if (remainingLetters === 0) {
+                    wins++;
+                    document.getElementById("wins").innerHTML = wins;
                 }
             }
-        } else {
-            remainingGuesses--;
-        }
-    }
+        } else if (l === -1) {
+        remainingGuesses--;
+        charactersGuessed.push(userGuess);
+        document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+        document.getElementById("chosenLetters").innerHTML = charactersGuessed;
+    
+}
+}
+
+
 
 //Need to show guessed letters that aren't correct 
 //Need to add to wins if word is guessed
